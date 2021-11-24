0931ea14323da78eb1adbd9c77c4dd1a0fe48c05
AVANADE-CORP\luciano.f.de.brito <luciano.f.de.brito@avanade.com>
2021-11-23 22:06:42 -0300

    Filters Logger Class and Interface LogMvcAction Filter

---
+++ EasyBlog.Web/Core/ILogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace EasyBlog.Web.Core
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class TraceLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Trace.WriteLine(message);
+        }
+    }
+}
