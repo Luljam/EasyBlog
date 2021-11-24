12e68a011f23a614dd8d5a4ec1e260668d9d03e4
AVANADE-CORP\luciano.f.de.brito <luciano.f.de.brito@avanade.com>
2021-11-23 21:06:07 -0300

    Criando e registrando Local Strings

---
+++ EasyBlog.Web/Core/LocalStrings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyBlog.Web.Core
+{
+    public class LocalStrings : ILocatorStrings
+    {
+        public string SiteTitle {
+            get { return "EasyBlog (DI Enable)";  } 
+        }
+        public string PostsTitle
+        {
+            get { return "Easy Blog - Home"; }
+        }
+
+        public string PostTitle
+        {
+            get { return "Blog Post"; }
+        }
+
+        public string NewPostTitle
+        {
+            get { return "New Blog Post"; }
+        }
+    }
+    public interface ILocatorStrings
+    {
+        string SiteTitle { get; }
+        string PostsTitle { get; }
+        string PostTitle { get; }
+        string NewPostTitle { get; }
+
+    }
+}
